2d7611561e87b6d44d2e48d7a39b625b0520b6a8
sptkl <caobaiyue@gmail.com>
2020-08-04 14:08:30 +0000

    consolidate cli functions

--- bin/config.sh
+++ bin/config.sh
@@ -56,3 +56,8 @@ function Upload {
   done
   wait
 }
+
+function setup {
+mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
+}
+register 'setup' '' 'install system dependencies' setup
--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-# #!/bin/bash
-
-function setup {
-mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
-}
-register 'setup' '' 'install system dependencies' setup
