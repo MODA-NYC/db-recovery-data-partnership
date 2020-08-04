eeea1521c343d2f01352d1c728a8ce758d76e70f
sptkl <caobaiyue@gmail.com>
2020-08-04 16:53:44 +0000

    adding local development cli

--- bin/run.sh
+++ bin/run.sh
@@ -4,3 +4,15 @@ function run {
     bash $(pwd)/recipes/$1/runner.sh
 }
 register 'run' 'recipe' '{ recipe name }' run
+
+function local_run {
+    docker run --rm\
+        -v $(pwd)/:/_w\
+        --user $UID\
+        -w /_w\
+        nycplanning/rdp:latest bash -c "
+        ./rdp setup
+        bash /_w/recipes/$1/runner.sh
+        "
+}
+register 'run' 'local' '{ recipe name }' local_run
