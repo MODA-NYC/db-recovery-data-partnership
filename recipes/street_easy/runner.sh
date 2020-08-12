50f7acf2cb18b5bc4ecdb155b90522f51d68163f
SPTKL <caobaiyue@gmail.com>
2020-08-12 12:08:11 -0400

    uncomment

--- recipes/street_easy/runner.sh
+++ recipes/street_easy/runner.sh
@@ -4,30 +4,30 @@ BASEDIR=$(dirname $0)
 VERSION=$DATE
 
 # StreetEasy NTA Level ETL
-# (
-#     cd $BASEDIR
-#     mkdir -p output
-#     NAME=$(basename $BASEDIR)
+(
+    cd $BASEDIR
+    mkdir -p output
+    NAME=$(basename $BASEDIR)
 
-#     python3 build.py | 
-#     psql $RDP_DATA -v NAME=$NAME -v VERSION=$VERSION -f create.sql
+    python3 build.py | 
+    psql $RDP_DATA -v NAME=$NAME -v VERSION=$VERSION -f create.sql
 
-#     (
-#         cd output
+    (
+        cd output
 
-#         # Export to CSV
-#         psql $RDP_DATA -c "\COPY (
-#             SELECT * FROM $NAME.\"$VERSION\"
-#         ) TO stdout DELIMITER ',' CSV HEADER;" > street_easy_nta.csv
+        # Export to CSV
+        psql $RDP_DATA -c "\COPY (
+            SELECT * FROM $NAME.\"$VERSION\"
+        ) TO stdout DELIMITER ',' CSV HEADER;" > street_easy_nta.csv
 
-#         # Export to ShapeFile
-#         SHP_export $RDP_DATA $NAME.latest MULTIPOLYGON street_easy_nta
+        # Export to ShapeFile
+        SHP_export $RDP_DATA $NAME.latest MULTIPOLYGON street_easy_nta
 
-#         # Write VERSION info
-#         echo "$VERSION" > version.txt
+        # Write VERSION info
+        echo "$VERSION" > version.txt
 
-#     )
-# ) 
+    )
+) 
 
 # StreetEasy Rental/Sales Indecies
 (
