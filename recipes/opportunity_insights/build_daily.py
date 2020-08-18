1a9d0172c6d5ac3fcf97f95882968ad7bb7c7b5b
mgraber <molly.graber@me.com>
2020-08-18 18:13:39 +0000

    Include date fields in index

--- recipes/opportunity_insights/build_daily.py
+++ recipes/opportunity_insights/build_daily.py
@@ -5,36 +5,39 @@ NYC_FIPS = ['36005','36047','36061','36081','36085']
 
 # Read daily data tables from GitHub
 df1 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/Affinity%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/Affinity%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 df2 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/COVID%20Cases%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/COVID%20Cases%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 df3 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/COVID%20Deaths%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/COVID%20Deaths%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 df4 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/Google%20Mobility%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/Google%20Mobility%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 df5 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/Womply%20Merchants%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/Womply%20Merchants%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 df6 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/Womply%20Revenue%20-%20County%20-%20Daily.csv",
-    dtype=str,
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/Womply%20Revenue%20-%20County%20-%20Daily.csv",
+    dtype=str
 )
 
 # Filter to NYC and set FIPS code as index
 dfs = [df1, df2, df3, df4, df5, df6]
-dfs = [df[df.countyfips.isin(NYC_FIPS)].set_index("countyfips", drop=True) for df in dfs]
+dfs = [df[df.countyfips.isin(NYC_FIPS)].set_index(["countyfips","year","month","day"], drop=True) for df in dfs]
+for df in dfs:
+    print(list(df))
+    print(df.head())
 
 # Concatenate tables and reset index
-merged = pd.concat(dfs, axis=1, keys=range(len(dfs)), join='outer', copy=False)
+merged = pd.concat(dfs, axis=1, join='outer', copy=False)
 merged.reset_index(drop=False, inplace=True)
 
 merged.to_csv('input/daily_raw.csv', index=False)
--- recipes/opportunity_insights/build_weekly.py
+++ recipes/opportunity_insights/build_weekly.py
@@ -5,20 +5,20 @@ NYC_FIPS = ['36005','36047','36061','36081','36085']
 
 # Read daily data tables from GitHub
 df1 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/Zearn%20-%20County%20-%20Weekly.csv",
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/Zearn%20-%20County%20-%20Weekly.csv",
     dtype=str,
 )
 df2 = pd.read_csv(
-    "https://github.com/OpportunityInsights/EconomicTracker/blob/main/data/UI%20Claims%20-%20County%20-%20Weekly.csv",
+    "https://raw.githubusercontent.com/OpportunityInsights/EconomicTracker/main/data/UI%20Claims%20-%20County%20-%20Weekly.csv",
     dtype=str,
 )
 
 # Filter to NYC and set FIPS code as index
 dfs = [df1, df2]
-dfs = [df[df.countyfips.isin(NYC_FIPS)].set_index("countyfips", drop=True) for df in dfs]
+dfs = [df[df.countyfips.isin(NYC_FIPS)].set_index(["countyfips","year","month","day_endofweek"], drop=True) for df in dfs]
 
 # Concatenate tables and reset index
-merged = pd.concat(dfs, axis=1, keys=range(len(dfs)), join='outer', copy=False)
+merged = pd.concat(dfs, axis=1, join='outer', copy=False)
 merged.reset_index(drop=False, inplace=True)
 
 merged.to_csv('input/weekly_raw.csv', index=False)
