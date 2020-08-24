f81ee3361169a1ec2b4e44a48daf2bd0fbee4c93
sptkl <caobaiyue@gmail.com>
2020-08-24 23:16:29 +0000

    finishing up foursquare datacube

--- recipes/foursquare/runner_datacube.sh
+++ recipes/foursquare/runner_datacube.sh
@@ -8,49 +8,70 @@ function foursquare_datacube {
         cd $BASEDIR
         NAME=foursquare_datacube
         
-        # mc cp $GSHEET_CRED creds.json
+        mc cp $GSHEET_CRED creds.json
         mkdir -p input && mkdir -p output
 
-        # python3 datacube.py
+        psql $RDP_DATA -f init.sql
 
-        (
-            cd input
+        python3 datacube.py
 
-            for file in $(ls *.tar.gz)
-            do
-                echo $file
+        if [ "$(ls -A input)" ]; then
+            (
+                cd input
 
-                VERSION=${file%%.*}
-                file_name=$(tar tf $file | grep .csv.gz)
-                tar -xvzf $file $file_name -O > $VERSION.csv.gz
+                for file in $(ls *.tar.gz)
+                do
+                    max_bg_procs 5
+                    (
+                        echo $file
+
+                        VERSION=${file%%.*}
+                        file_name=$(tar tf $file | grep .csv.gz)
+                        tar -xvzf $file $file_name -O > $VERSION.csv.gz
+                        
+                        gunzip -dc $VERSION.csv.gz | 
+                        psql $RDP_DATA \
+                            -v NAME=$NAME \
+                            -v VERSION=$VERSION \
+                            -f ../create_datacube.sql
+                        rm $file
+                        rm $VERSION.csv.gz
+                    ) &
+                done
+            )
+
+            (
+                cd output
                 
-                gunzip -dc $VERSION.csv.gz | 
-                psql $RDP_DATA \
-                    -v NAME=$NAME \
-                    -v VERSION=$VERSION \
-                    -f ../create_datacube.sql
-            done
-        )
+                VERSION=$(
+                    psql $RDP_DATA -At -c "
+                    SELECT MAX(table_name::date) 
+                    FROM information_schema.tables 
+                    where table_schema = 'foursquare_datacube'
+                    AND table_name !~* 'latest|main'"
+                )
 
-        # (
-        #     cd output
-            
-        #     # Export to CSV
-        #     psql $RDP_DATA -c "\COPY (
-        #         SELECT * FROM $NAME.\"$VERSION\"
-        #     ) TO stdout DELIMITER ',' CSV HEADER;" > $NAME.csv
+                # Export to CSV
+                psql $RDP_DATA -c "\COPY (
+                    SELECT * FROM $NAME.latest
+                ) TO stdout DELIMITER ',' CSV HEADER;" > $NAME.csv
 
-        #     psql $RDP_DATA -c "\COPY (
-        #         SELECT * FROM $NAME.latest
-        #     ) TO stdout DELIMITER ',' CSV HEADER;" > foursquare_datacube_nyc.csv
+                psql $RDP_DATA -c "\COPY (
+                    SELECT * FROM $NAME.grouped_latest
+                ) TO stdout DELIMITER ',' CSV HEADER;" > foursquare_datacube_grouped.csv
 
-        #     # Write VERSION info
-        #     echo "$VERSION" > version.txt
-        # )
-
-        # Upload $NAME $VERSION
-        # Upload $NAME latest
-        # rm -rf output && rm -rf output
-        # rm creds.json
+                # Write VERSION info
+                echo "$VERSION" > version.txt
+            )
+            VERSION=$(cat output/version.txt)
+            Upload $NAME $VERSION
+            Upload $NAME latest
+            rm -rf input && rm -rf output
+            rm creds.json
+        else
+            echo "the database is up-to-date!"
+            rm -rf input && rm -rf output
+            rm creds.json
+        fi
     )
 }
