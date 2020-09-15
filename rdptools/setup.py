f84bce46f2ea577f07fee1c43b2044617b14c90c
sptkl <caobaiyue@gmail.com>
2020-09-15 13:00:07 -0400

    finalize distribution scripts

--- rdptools/setup.py
+++ rdptools/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='rdptools',
-      version='0.1.1',
+      version='0.1.4',
       description='recovery data partnership python package',
       pacakges=find_packages(),
       install_requires=[
           'pandas',
-          'Office365-REST-Python-Client==2.2.0'
+          'office365-rest-client=2.2.0'
       ]
     )
