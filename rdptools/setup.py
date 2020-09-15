cb037584f1a920066362a808202e0ea00915b449
sptkl <caobaiyue@gmail.com>
2020-09-15 11:26:30 -0400

    first commit to rdptools

---
+++ rdptools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='rdptools',
+      version='0.1.0',
+      description='recovery data partnership python package',
+      pacakges=find_packages(),
+      install_requires=[
+          'pandas',
+          'office365-rest-client==2.2.0'
+      ]
+    )
