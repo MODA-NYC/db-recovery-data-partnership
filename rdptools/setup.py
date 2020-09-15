3011bd99daa3ac3af23d0f18e35994caa0b488f4
sptkl <caobaiyue@gmail.com>
2020-09-15 13:29:01 -0400

    improving documentation and upload rdptools==0.1.5

--- rdptools/setup.py
+++ rdptools/setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='rdptools',
-      version='0.1.4',
+      version='0.1.5',
       description='recovery data partnership python package',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       pacakges=find_packages(),
       install_requires=[
           'pandas',
