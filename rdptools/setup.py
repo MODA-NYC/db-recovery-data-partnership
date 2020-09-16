34203dd7f6789d29b8735d94abb1c0e690c72e62
sptkl <caobaiyue@gmail.com>
2020-09-16 12:58:14 -0400

    adding spatial boundary files

--- rdptools/setup.py
+++ rdptools/setup.py
@@ -2,18 +2,15 @@ from setuptools import setup, find_packages
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='rdptools',
-      version='0.1.5',
-      description='recovery data partnership python package',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      pacakges=find_packages(),
-      install_requires=[
-          'pandas',
-          'office365-rest-client==2.2.0',
-          'geopandas'
-      ]
-    )
+setup(
+    name="rdptools",
+    version="0.1.6",
+    description="recovery data partnership python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    pacakges=find_packages(),
+    install_requires=["pandas", "office365-rest-client==2.2.0", "geopandas"],
+)
--- rdptools/tests/test_partner.py
+++ rdptools/tests/test_partner.py
@@ -3,31 +3,31 @@ from rdptools.core import Site
 from dotenv import load_dotenv
 import os
 
+
 class TestPartnerMethods(unittest.TestCase):
     def setUp(self):
         load_dotenv()
-        self.site_url = os.environ.get('SITE_URL')
-        username = os.environ.get('USERNAME')
-        password = os.environ.get('PASSWORD')
+        self.site_url = os.environ.get("SITE_URL")
+        username = os.environ.get("USERNAME")
+        password = os.environ.get("PASSWORD")
         rdp = Site(self.site_url, username, password)
-        self.partner = rdp.create_partner('betanyc')
+        self.partner = rdp.create_partner("betanyc")
 
     def test_SiteRoot(self):
-        SiteRoot=self.partner.get_SiteRoot()
-        _SiteRoot='/'+'/'.join(self.site_url.split('/')[-2:])+'/'
+        SiteRoot = self.partner.SiteRoot
+        _SiteRoot = "/" + "/".join(self.site_url.split("/")[-2:]) + "/"
         self.assertEqual(SiteRoot, _SiteRoot)
 
     def test_list_versions(self):
-        versions=self.partner.list_versions()
-        latest_versions=self.partner.list_latest()
-        for i in versions:
-            self.assertIn(i, latest_versions)
+        versions = self.partner.list_versions()
+        self.assertNotEqual(len(versions), 0)
 
     def test_list_files(self):
-        files=self.partner.list_files()
+        files = self.partner.list_files()
         for _file in files:
-            ext=os.path.splitext(_file)[-1]
-            self.assertIn(ext, ['.txt', '.zip', '.csv'])
+            ext = os.path.splitext(_file["fileName"])[-1]
+            self.assertIn(ext, [".txt", ".zip", ".csv"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
