8f0a22a7136d5e7649225f3340b07185c9596f37
mgraber <molly.graber@me.com>
2020-09-16 12:27:29 +0000

    Map columns for kinsa

--- recipes/kinsa/create.sql
+++ recipes/kinsa/create.sql
@@ -12,7 +12,26 @@ CREATE TEMP TABLE tmp (
 
 CREATE SCHEMA IF NOT EXISTS :NAME;
 DROP TABLE IF EXISTS :NAME.:"VERSION" CASCADE;
-SELECT * INTO :NAME.:"VERSION" FROM tmp;
+SELECT
+    (CASE 
+        WHEN a.region_name ~* 'New York' THEN 'MN'
+        WHEN a.region_name ~* 'Bronx' THEN 'BX'
+        WHEN a.region_name ~* 'Kings' THEN 'BK'
+        WHEN a.region_name ~* 'Queens' THEN 'QN'
+        WHEN a.region_name ~* 'Richmond' THEN 'SI'
+    END) as borough,
+    (CASE 
+        WHEN a.region_name ~* 'New York' THEN 1
+        WHEN a.region_name ~* 'Bronx' THEN 2
+        WHEN a.region_name ~* 'Kings' THEN 3
+        WHEN a.region_name ~* 'Queens' THEN 4
+        WHEN a.region_name ~* 'Richmond' THEN 5
+    END) as borocode,
+    a.region_id as borofips,
+    a.created_date as date,
+    a.percent_ill
+INTO :NAME.:"VERSION" 
+FROM tmp a;
 
 DROP VIEW IF EXISTS :NAME.latest;
 CREATE VIEW :NAME.latest AS (
