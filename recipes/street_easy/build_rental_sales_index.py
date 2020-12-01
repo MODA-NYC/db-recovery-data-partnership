9ca2d11a40dd5eeeab081728bdcbdd4af7a1d396
mgraber <molly.graber@me.com>
2020-12-01 16:55:19 +0000

    Modify expected URL_STREET_EASY to match PR#167

--- recipes/street_easy/build_rental_sales_index.py
+++ recipes/street_easy/build_rental_sales_index.py
@@ -3,20 +3,24 @@ import sys
 import datetime
 import pandas as pd
 
+BASE_URL='https://streeteasy-market-data-download.s3.amazonaws.com'
 URL_STREET_EASY=os.environ['URL_STREET_EASY']
+URL=f'{BASE_URL}/{URL_STREET_EASY}'
+
+# Calculate last month
 this_month = datetime.date.today().replace(day=1)
 last_month = this_month - datetime.timedelta(days=1)
 VERSION = last_month.strftime("%Y-%m")
 
 try:
-    url = f"{URL_STREET_EASY}price_indices-{VERSION}.csv"
+    url = f"{URL}/price_indices-{VERSION}.csv"
     all_rows = pd.read_csv(url, dtype=str)
 except:
     last_month = last_month.replace(day=1)
     PREV_VERSION = (last_month.replace(month=last_month.month-1)).strftime("%Y-%m")
 
     # Data for {VERSION} is not available yet, trying {PREV_VERSION}
-    url = f"{URL_STREET_EASY}price_indices-{PREV_VERSION}.csv"
+    url = f"{URL}/price_indices-{PREV_VERSION}.csv"
     all_rows = pd.read_csv(url, dtype=str)
 
 r = all_rows[all_rows['TYPE']=='rentals'].rename(columns={"index": "rental_index"})
