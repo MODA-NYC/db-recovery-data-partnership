f732f145489a15137b858814edc4795c5a13c38c
Will Hsiao <newbiegg@gmail.com>
2020-12-10 15:50:22 +0000

    test from will

--- recipes/street_easy/build_rental_sales_index.py
+++ recipes/street_easy/build_rental_sales_index.py
@@ -14,21 +14,7 @@ sales_index = pd.read_csv(
 )
 
 # Unpivotting tables
-r = (
-    rent_index[["Brooklyn", "Manhattan", "NYC", "Queens"]]
-    .stack()
-    .reset_index()
-    .rename(columns={"level_1": "borough", 0: "rental_index"})
-)
 
-s = (
-    sales_index[["Brooklyn", "Manhattan", "NYC", "Queens"]]
-    .stack()
-    .reset_index()
-    .rename(columns={"level_1": "borough", 0: "sales_index"})
-)
-
-df = s.merge(r, on=["Month", "borough"], how="outer")
 df.to_csv(
     sys.stdout, index=False, columns=["Month", "borough", "sales_index", "rental_index"]
 )
