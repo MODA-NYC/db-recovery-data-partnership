af30c7a1e63ae5abb4ace5c79fb2c0b470604b62
SteveScott <sscott1@cityops.nyc.gov>
2021-04-06 16:26:42 +0000

    removed debugging which was throwing an error in axway_ls. Also disabled errexit option so it wont exit when there is an error.

--- bin/axway.sh
+++ bin/axway.sh
@@ -5,14 +5,14 @@ source $(pwd)/bin/cli.sh
 set -o pipefail  # trace ERR through pipes
 set -o errtrace  # trace ERR through 'time command' and other functions
 #set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+#set -o errexit   ## set -e : exit the script if any statement returns a non-false return value
 
 function axway_ls {
-    chmod 700 ~/.ssh/id_rsa_axway
+    #chmod 700 ~/.ssh/id_rsa_axway
     sftp -i ~/.ssh/id_rsa_axway \
     -o StrictHostKeyChecking=no \
-    $AXWAY_USER@$AXWAY_HOST || echo 'error in axway_ls' << EOF
-    ls $@ || echo 'error listing'
+    $AXWAY_USER@$AXWAY_HOST << EOF
+    ls $@ 
 EOF
 }
 register 'axway' 'ls' 'listing directory' axway_ls
