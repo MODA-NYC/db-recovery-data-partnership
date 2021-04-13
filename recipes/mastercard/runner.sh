4f2c7a75172e9d344a5d5ef2851df6f36c394986
SteveScott <sscott1@cityops.nyc.gov>
2021-04-13 20:07:37 +0000

    initial commit to mastercard

--- recipes/mastercard/runner.sh
+++ recipes/mastercard/runner.sh
@@ -9,12 +9,12 @@ VERSION=$DATE
     cd $BASEDIR
     mkdir -p input
     mkdir -p output
-    
+
     #pull the directory
-    
+
     #extract
     unzip -d /input -P $MASTERCARD_PASSWORD $(find $BASEDIR -name "*.zip" | head -1)
-    
+
     #modify csv in python
     (ulimit -t 300
     bash -x 
@@ -23,6 +23,6 @@ VERSION=$DATE
     #clean-up
     rm -f -r input output
 
-    
 
-)
+
+) 
