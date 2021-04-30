308f78c8cf97146697e2d6399f5a01e31d84db50
SteveScott <sscott1@cityops.nyc.gov>
2021-04-30 16:31:11 +0000

    pulls successfully from Mastercard. Should work but needs verification

--- recipes/mastercard/runner.sh
+++ recipes/mastercard/runner.sh
@@ -10,23 +10,27 @@ VERSION=$DATE
     cd $BASEDIR
     mkdir -p input
     mkdir -p output
-
+    chmod 777 input
     
     #For testing purposes
-    #unzip -d /input -P $MASTERCARD_PASSWORD $(find $BASEDIR -name "*.zip" |
-    #head -1 ) 
-
+    #cp Geogrids_NYC_Zip_Code_Level_Jan2019_Feb2021.zip input/
 
     #will download all files from mastercard. Then mastercard will delete after successfull download.
-   
-    scp newyorkcity@files.mastercard.com:geoinsights/data/fromMC/* /input
-    unzip -P $MASTERCARD_PASSWORD $(find $BASEDIR/input -name "*.zip" | head -1 )
-    cd $BASEDIR 
-    
-    #find the filename. Assumes one file.
+    scp -v -P 22022  -i /root/.ssh/id_rsa_axway -o "StrictHostKeyChecking=no" newyorkcity@files.mastercard.com:geoinsights/data/fromMC/* /input
+    echo 'file is ' $(find $BASEDIR/input -name "*.zip" ) 
+    unzip -d /input -P $MASTERCARD_PASSWORD $(find $BASEDIR/input -name "*.zip" | head -1 ) 
+    rm $(find $BASEDIR/input -name "*.zip")
+    #cd $BASEDIR
+
+    #find the filename. Assumes one file. (all compressed files removed, and head selects ony one file,
+    # so there should only be on csv found)
     KEY=$(ls /input)
     FILENAME=$(basename $KEY)
     echo $FILENAME
+
+    #upload file to aws: InvalidAccessKeyId error, AWS access key already assigned to the digital ocean account. Need to switch accounts.
+    #aws s3 cp /input/$KEY s3://recovery-data-partnership/mastercard/
+    
     #send csv to PSQL
     cat /input/$FILENAME | psql $RDP_DATA -v NAME=$NAME -v VERSION=$VERSION -f create_mastercard.sql
     #clean up
@@ -40,17 +44,18 @@ VERSION=$DATE
         #Write Version info
         echo "$VERSION" > version.txt
     )
-    #can to split the CSVs. Makes a mess.
+    #can to split the CSVs, but makes a mess.
     #cat output/mastercard.csv | python3 split_csv.py
     
     #unsplit csv is too large. Must compress.
     zip -9 output/mastercard_$DATE.zip output/mastercard.csv
     
-    #If you don't remove unsplit csv, sharepoint.py will overflow the RAM and the process killed.
+    #If you don't remove unsplit csv, sharepoint.py will overflow the RAM and the process killed when it tries to upload it.
+    #Upload uploads everything in the output folder.
     rm -rf output/mastercard.csv
     
     Upload $NAME $VERSION
     Upload $NAME latest
     rm -rf output
     Version $NAME '' $VERSION $NAME
-) 
+)
