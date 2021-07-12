1e829b23823aa4f5cddacc92cf7bd74d5f505b77
SteveScott <stevescott517@gmail.com>
2021-07-12 14:28:29 -0400

    change root to ~
    change root to home

--- recipes/mastercard/runner.sh
+++ recipes/mastercard/runner.sh
@@ -38,7 +38,7 @@ AWS_DEFAULT_REGION=us-east-1
     
     #will download all files from mastercard. Then mastercard will delete after successfull download. May be more than one file without check.
     echo 'downloading from mastercard'
-    scp -P 22022 -i /root/.ssh/id_rsa_axway -o "StrictHostKeyChecking=no" newyorkcity@files.mastercard.com:geoinsights/data/fromMC/* ./input
+    scp -P 22022 -i ~/.ssh/id_rsa_axway -o "StrictHostKeyChecking=no" newyorkcity@files.mastercard.com:geoinsights/data/fromMC/* ./input
     #For testing purposes
     #cp Geogrids_NYC_Zip_Codes_Level_01Jan2019_25Apr2021_Final.zip input/
     
