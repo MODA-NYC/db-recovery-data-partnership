8e67d0bd5c7b5d5af4a2bcbac5160ca533ae51d9
SteveScott <sscott1@cityops.nyc.gov>
2021-10-15 15:08:00 +0000

    Inserted bash script into python to get VERSION from db

--- recipes/foursquare/asof.py
+++ recipes/foursquare/asof.py
@@ -1,7 +1,9 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import sys
 
+RDP_DATA = os.getenv("RDP_DATA")
 # To simplify credential management we will identify the latest version 
 # name by heading to the data website: https://visitdata.org/data-noncommercial
 # version info (asof) is listed e.g. "Raw data files for data version 20200726-v0:"
@@ -9,12 +11,14 @@ import sys
 # https://data.visitdata.org/processed/vendor/foursquare/asof/20200726-v0/NewYork_StatenIsland.csv
 
 #visitdata.org is no longer a functional Site!
-VERSION=$(
-    psql $RDP_DATA -At -c "
+VERSION=os.system(
+    f'''
+    psql {RDP_DATA} -At -c "
     SELECT MAX(table_name::date) 
     FROM information_schema.tables 
     where table_schema = 'foursquare_zipcode'
     AND table_name !~* 'latest|main|zipcode';"
+    '''         
              )
 '''
 site_url = 'https://visitdata.org/data-noncommercial'
@@ -22,5 +26,5 @@ html_doc = requests.get(site_url).content
 soup = BeautifulSoup(html_doc, 'html.parser')
 asof = soup.find_all('h2')[0].string[-12:-1]
 '''
-asof = $VERSION
+asof = VERSION
 print(asof, file=sys.stdout)
