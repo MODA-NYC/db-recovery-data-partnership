dee80bab5facade37f45da1888e3b9462eca2aca
SteveScott <sscott1@cityops.nyc.gov>
2021-10-15 18:56:48 +0000

    just going to add a default value of 01-01-1900 and see what happens

--- recipes/foursquare/asof.py
+++ recipes/foursquare/asof.py
@@ -7,9 +7,12 @@ from sys import argv
 
 def main():
     #supposedly asof is needed for workflow and github actions?
+    '''
+    #having trouble with SQL Alchemy parsing environmental variable
     zipcode_or_county = argv[0]
     RDP_DATA = os.getenv("RDP_DATA_SQL_ALCHEMY", "RDP_DATA_SQL_ALCHEMY environment variable does not exist!")
     engine = create_engine(RDP_DATA)
+    '''
     # To simplify credential management we will identify the latest version 
     # name by heading to the data website: https://visitdata.org/data-noncommercial
     # version info (asof) is listed e.g. "Raw data files for data version 20200726-v0:"
@@ -17,21 +20,18 @@ def main():
     # https://data.visitdata.org/processed/vendor/foursquare/asof/20200726-v0/NewYork_StatenIsland.csv
 
     #visitdata.org is no longer a functional Site!
-
+    '''
+    #can't get engine to work
     loaded=pd.read_sql(sql='''
-        SELECT table_name 
-        FROM information_schema.tables 
-        WHERE table_schema = 'foursquare_{}'
-        AND table_name not in ('main', 'latest')
+        #SELECT table_name 
+        #FROM information_schema.tables 
+        #WHERE table_schema = 'foursquare_{}'
+        #AND table_name not in ('main', 'latest')
     '''.format(zipcode_or_county), con=engine)
     loaded_dates=loaded.table_name.to_list()
     VERSION = max(loaded_dates)
     '''
-    site_url = 'https://visitdata.org/data-noncommercial'
-    html_doc = requests.get(site_url).content
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    asof = soup.find_all('h2')[0].string[-12:-1]
-    '''
+    VERSION = "01-01-1900"
     asof = VERSION
     print(asof, file=sys.stdout)
     #raise(Exception("stopping here"))
