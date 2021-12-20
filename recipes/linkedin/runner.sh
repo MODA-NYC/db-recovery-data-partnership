d05ceb33359d5365bff3c2fac9c50ac6d261c1f4
SteveScott <stevescott517@gmail.com>
2021-12-20 16:22:23 -0500

    comment out rest of code if I can't throw.

--- recipes/linkedin/runner.sh
+++ recipes/linkedin/runner.sh
@@ -18,24 +18,24 @@ pip install openpyxl
     rm -rf input/raw.xlsx
     axway_cmd get $latest_file input/raw.xlsx
 
-    python3 build.py | throw();
-    psql $RDP_DATA -v NAME=$NAME -v VERSION=$VERSION -f create.sql
-    rm -rf input
-
-    (
-        cd output
-
-        # Export to CSV
-        psql $RDP_DATA -c "\COPY (
-            SELECT * FROM linkedin.\"$VERSION\"
-        ) TO stdout DELIMITER ',' CSV HEADER;" > linkedin_nyc_hiringrate.csv
-
-        # Write VERSION info
-        echo "$VERSION" > version.txt
-    )
-    
-    Upload $NAME $VERSION
-    Upload $NAME latest
-    rm -rf output
-    Version $NAME '' $VERSION $NAME
+    python3 build.py | 
+    #psql $RDP_DATA -v NAME=$NAME -v VERSION=$VERSION -f create.sql
+    #rm -rf input
+    #
+    #(
+    #    cd output
+    #
+    #    # Export to CSV
+    #    psql $RDP_DATA -c "\COPY (
+    #        SELECT * FROM linkedin.\"$VERSION\"
+    #    ) TO stdout DELIMITER ',' CSV HEADER;" > linkedin_nyc_hiringrate.csv
+    #
+    #    # Write VERSION info
+    #    echo "$VERSION" > version.txt
+    #)
+    #
+    #Upload $NAME $VERSION
+    #Upload $NAME latest
+    #rm -rf output
+    #Version $NAME '' $VERSION $NAME
 )
