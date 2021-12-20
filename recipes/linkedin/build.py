4b3ca6bc7e1a63dc07b023efc7fc70f973d9b581
SteveScott <stevescott517@gmail.com>
2021-12-20 17:18:12 -0500

    one more try to read excel file.

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -9,7 +9,9 @@ if raw_xl_path.is_file():
     print("'raw.xlsx does in fact exist.")
 else:
     raise Exception("raw.xlsx not found!")
-df = pd.read_excel(raw_xl_path), 0)
+#df = pd.read_excel(raw_xl_path), 0)
+xlsx = pd.ExcelFile(raw_xl_path)
+df = pd.read_excel(xlsx, 'Data')
 print(df.info())
 cols = ['month_begin_date','hiring_rate_sa','mom_change','yoy_change']
 for col in cols:
