349ccf27967e02b4769787d771e18a89cd7149e5
SteveScott <stevescott517@gmail.com>
2021-12-20 16:59:04 -0500

    try sheet 1

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -4,7 +4,7 @@ import pandas as pd
 import sys
 from pathlib import Path
 
-df = pd.read_excel(os.path.join(os.getcwd(), 'input', 'raw.xlsx'), 0)
+df = pd.read_excel(os.path.join(os.getcwd(), 'input', 'raw.xlsx'), 1)
 print(df.info())
 cols = ['month_begin_date','hiring_rate_sa','mom_change','yoy_change']
 for col in cols:
