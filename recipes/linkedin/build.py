2e49d2fc197ce296f773724d1d316cf4c6bf5678
SteveScott <stevescott517@gmail.com>
2021-12-20 17:20:21 -0500

    is_file not working.

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -5,10 +5,10 @@ import sys
 from pathlib import Path
 
 raw_xl_path = os.path.join(os.getcwd(), 'input', 'raw.xlsx')
-if raw_xl_path.is_file():
-    print("'raw.xlsx does in fact exist.")
-else:
-    raise Exception("raw.xlsx not found!")
+#if raw_xl_path.is_file():
+#    print("'raw.xlsx does in fact exist.")
+#else:
+#    raise Exception("raw.xlsx not found!")
 #df = pd.read_excel(raw_xl_path), 0)
 xlsx = pd.ExcelFile(raw_xl_path)
 df = pd.read_excel(xlsx, 'Data')
