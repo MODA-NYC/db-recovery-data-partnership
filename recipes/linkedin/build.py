fa99941bd5871394be126cfdda7475baae1e8c2b
SteveScott <stevescott517@gmail.com>
2021-12-20 16:06:45 -0500

    stop here added for testing

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sys
 
 df = pd.read_excel('input/raw.xlsx', sheet_name='Data')
+print(df.info())
+raise Exception('stop here')
 cols = ['month_begin_date','hiring_rate_sa','mom_change','yoy_change']
 for col in cols:
     assert col in df.columns
