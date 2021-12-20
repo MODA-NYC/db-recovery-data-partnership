dbcad9ce1d61f7888b935dcdd5d7a15482a0b477
SteveScott <stevescott517@gmail.com>
2021-12-20 16:11:23 -0500

    check if raw.xlsx exists.

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -1,9 +1,12 @@
+from os.path import exists
 import pandas as pd
 import sys
 
+if exists('input/raw.xlsx'):
+    print("raw.xlsx in fact exists")
+else:
+    raise Exception("'raw.xlsx' does not exist!")
 df = pd.read_excel('input/raw.xlsx', sheet_name='Data')
-print(df.info())
-raise Exception('stop here')
 cols = ['month_begin_date','hiring_rate_sa','mom_change','yoy_change']
 for col in cols:
     assert col in df.columns
