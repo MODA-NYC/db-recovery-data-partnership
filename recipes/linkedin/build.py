12a8075960465448b4196d7a6e24c6b8e4c22ede
SteveScott <stevescott517@gmail.com>
2021-12-20 16:37:41 -0500

    rb takes 'rb'. fixed parens.

--- recipes/linkedin/build.py
+++ recipes/linkedin/build.py
@@ -6,7 +6,7 @@ if exists('input/raw.xlsx'):
     print("raw.xlsx in fact exists")
 else:
     raise Exception("'raw.xlsx' does not exist!")
-df = pd.read_excel((open('input/raw.xlsx'), rb), sheetname='Data')
+df = pd.read_excel(open('input/raw.xlsx', 'rb'), sheetname='Data')
 cols = ['month_begin_date','hiring_rate_sa','mom_change','yoy_change']
 for col in cols:
     assert col in df.columns
