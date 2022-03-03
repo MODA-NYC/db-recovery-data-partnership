247016f758fb3bad2b1edd074b135ce18507d5c1
SteveScott <5157577+SteveScott@users.noreply.github.com>
2022-03-03 13:39:17 -0500

    Update process_mastercard_manual_run.py
    changed naming convention mastercard.

--- recipes/mastercard/process_mastercard_manual_run.py
+++ recipes/mastercard/process_mastercard_manual_run.py
@@ -7,10 +7,12 @@ import os, copy
 def reduce_csv(csv_name):
     df = pd.read_csv(csv_name, delimiter='|')
     zipcodes = pd.read_csv(Path.cwd() / 'zipcode.csv')
+    max_date = df['txn_date'].max()
+    min_date = df['txn_date'].min()
+    print("f{min_date} to {max_date}")
     df = df.rename({'txn_amt':'txn_amt_index', 'txn_cnt':'txn_cnt_index', 'acct_cnt':'acct_cnt_index', 'avg_ticket':'avg_ticket_index', 'avg_freq':'avg_freq_index', 'avg_spend_amt':'avg_spend_amt_index'}, axis='columns')
     df = df.merge(zipcodes[['borough', 'borocode', 'zip']], how='inner', left_on='Zip_code', right_on='zip')
     df = df.drop(columns='zip')
-    #print(df.info())
-    df.to_csv('daily_transactions_' + csv_name )
+    df.to_csv(f"mastercard_{min_date}_to_{max_date}.csv" )
     return df
 
