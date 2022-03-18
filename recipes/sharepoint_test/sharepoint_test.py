7c1a250bef7727871c1d14096ca01532af2d315f
SteveScott <5157577+SteveScott@users.noreply.github.com>
2022-03-18 10:47:35 -0400

    Create sharepoint_test.py

---
+++ recipes/sharepoint_test/sharepoint_test.py
@@ -0,0 +1,24 @@
+
+
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.client_credential import ClientCredential
+import sys
+import os
+import glob
+from io import StringIO, BytesIO
+import pandas as pd
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+settings=dict(
+        url=os.environ['SHAREPOINT_URL'],
+        client_credentials=dict(
+            client_id=os.environ.get('SHAREPOINT_CLIENT_ID', ''),
+            client_secret=os.environ.get('SHAREPOINT_CLIENT_SECRET', '')
+        )
+    )
+
+ctx = ClientContext(settings['url']).with_credentials(
+ClientCredential(settings['client_credentials']['client_id'],
+                 settings['client_credentials']['client_secret']))
