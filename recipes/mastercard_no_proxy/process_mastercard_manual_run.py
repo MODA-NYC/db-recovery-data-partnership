31f6712d2b820c735ce32e7b9334d2c3ebe4f61f
SteveScott <sscott1@cityops.nyc.gov>
2022-06-10 18:15:15 -0400

    test emptiness of dataframe

--- recipes/mastercard_no_proxy/process_mastercard_manual_run.py
+++ recipes/mastercard_no_proxy/process_mastercard_manual_run.py
@@ -15,6 +15,11 @@ def reduce_csv(csv_name):
     df = df.rename({'txn_amt':'txn_amt_index', 'txn_cnt':'txn_cnt_index', 'acct_cnt':'acct_cnt_index', 'avg_ticket':'avg_ticket_index', 'avg_freq':'avg_freq_index', 'avg_spend_amt':'avg_spend_amt_index'}, axis='columns')
     df = df.merge(zipcodes[['borough', 'borocode', 'zip']], how='inner', left_on='Zip_code', right_on='zip')
     df = df.drop(columns='zip')
+    try: 
+        assert(df.empty == False)
+        print("dataframe is not empty. Yay!")
+    except:
+        print("dataframe is empty :( ")
     filename = f"mastercard_{min_date}_to_{max_date}.csv"
     df.to_csv(filename)
     shutil.move(Path.cwd() / filename, Path.cwd() / 'output' / filename)
